a85d164e2db7c68801978ee6791b77bb6cb671a9
MrZorg111 <yodji333@mail.ru>
2024-11-10 20:49:47 +0300

    Непонятная ошибка(

---
+++ build/src/CMakeFiles/Lesson_36_QT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson_36_QT.dir/main.cpp.obj"
+  "CMakeFiles/Lesson_36_QT.dir/main.cpp.obj.d"
+  "Lesson_36_QT.exe"
+  "Lesson_36_QT.exe.manifest"
+  "Lesson_36_QT.pdb"
+  "libLesson_36_QT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lesson_36_QT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
